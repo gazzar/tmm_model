2ef635c3ac39dd0b4b5a469b9bd167dee237f359
Gary Ruben <gary.ruben@gmail.com>
2014-10-20 15:17:18 +1100

    Separated specification of yaml file and added absorption-enabling/disabling flags

--- commands/project.py
+++ commands/project.py
@@ -36,6 +36,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('filepattern', action='store',
                     help=textwrap.dedent('''e.g. a-*.tiff reads
                                           a-Ca.tiff, a-Zn.tiff, etc.'''))
+parser.add_argument('yamlfile', action='store',
+                    help='yamlfile e.g. golosio.yaml')
 parser.add_argument('-t', '--algorithm', action='store', default='r',
                     choices=['a','f','r','c'],
                     help=textwrap.dedent(
@@ -49,15 +51,25 @@ parser.add_argument('-s', '--scale', type=float, default=10.0,
                     help='scale (um/px) (default 10.0)')
 parser.add_argument('-e', '--energy', type=float, default=15.0,
                     help='energy (keV) (default 15.0)')
+parser.add_argument('--no-in-absorption', dest='no_in_absorption',
+                    default=False, action='store_true',
+                    help='disables incident absorption')
+parser.add_argument('--no-out-absorption', dest='no_out_absorption',
+                    default=False, action='store_true',
+                    help='disables outgoing absorption')
 
 args = vars(parser.parse_args())
 
 
 filepattern = args['filepattern']
+yamlfile = args['yamlfile']
 algorithm = args['algorithm']
 anglelist = args['anglelist']
 scale = args['scale']
 energy = args['energy']
+no_in_absorption = args['no_in_absorption']
+no_out_absorption = args['no_out_absorption']
 
-p = phantom.Phantom2d(filename=filepattern, um_per_px=scale, energy=energy)
-projection.project(p, algorithm, anglelist)
+p = phantom.Phantom2d(filename=filepattern, yamlfile=yamlfile,
+                      um_per_px=scale, energy=energy)
+projection.project(p, algorithm, anglelist, no_in_absorption, no_out_absorption)
